49189fd76ee4aee87c59d52cb02c522ed5e3b493
Jason Bell <jasonbell143@gmail.com>
2024-08-23 16:06:45 +0100

    set up testing and dependencies

---
+++ __tests__/app.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+
+const app = require("../app");
+
+// describe("GET /api/topics", () => {
+//   test("200: gets an array of all topics", () => {
+//     expect()
+//   });
+// });
