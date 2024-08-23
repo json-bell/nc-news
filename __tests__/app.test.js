18b8729135e962fdc11331c963393092c7e3c506
Jason Bell <jasonbell143@gmail.com>
2024-08-23 16:31:46 +0100

    Solved GET /api/topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,27 @@
 const request = require("supertest");
 
 const app = require("../app");
+const db = require("../db/connection");
 
-// describe("GET /api/topics", () => {
-//   test("200: gets an array of all topics", () => {
-//     expect()
-//   });
-// });
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+
+beforeEach(() => seed(data));
+afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  test("200: returns an array of all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3);
+        topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+        });
+      });
+  });
+});
