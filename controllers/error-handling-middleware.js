3df8a0703f54381baf30389c29ced5154acd1407
Jason Bell <jasonbell143@gmail.com>
2024-08-23 21:40:46 +0100

    refactored and added error handling for a not found URL

--- controllers/error-handling-middleware.js
+++ controllers/error-handling-middleware.js
@@ -1,4 +1,9 @@
-exports.defaultCatch = (err, req, res, next) => {
+exports.throwEndpointNotFound = (req, res, next) => {
+  if (true) res.status(404).send({ msg: "Endpoint not found" });
+  else next(err);
+};
+
+exports.handleErrorDefault = (err, req, res, next) => {
   console.log("Default error:", err);
   res.status(500).send({ msg: "Default error" });
 };
