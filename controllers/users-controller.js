0ac42466790f0e651bf50c9f338d8a181bf2c3bd
Jason Bell <jasonbell143@gmail.com>
2024-08-28 16:26:19 +0100

    solved and tested get-user-by-username

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,12 @@
-const { selectUsers } = require("../models/users-model");
+const { selectUsers, selectUserByUsername } = require("../models/users-model");
 
 exports.getUsers = (req, res, next) => {
   selectUsers().then((users) => res.status(200).send({ users }));
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUserByUsername(username)
+    .then((user) => res.status(200).send({ user }))
+    .catch((err) => next(err));
 };
