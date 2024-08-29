5f5d1abf37e59b7577ff0e9cfbf7bf1cf8d993d5
Jason Bell <jasonbell143@gmail.com>
2024-08-29 11:20:33 +0100

    solved get-comment-by-id

--- models/comments-model.js
+++ models/comments-model.js
@@ -27,6 +27,18 @@ exports.insertComment = (article_id, { username, body }) => {
     .then(({ rows }) => rows[0]);
 };
 
+exports.selectCommentById = (comment_id) => {
+  return checkExists("comments", "comment_id", comment_id)
+    .then(() =>
+      db.query(
+        `SELECT * FROM comments
+        WHERE comment_id = $1`,
+        [comment_id]
+      )
+    )
+    .then(({ rows }) => rows[0]);
+};
+
 exports.removeComment = (comment_id) => {
   return checkExists("comments", "comment_id", comment_id).then(() =>
     db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id])
