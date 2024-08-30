1e6dac36f0b44e358eea65a5dc1124bf969329ca
Jason Bell <jasonbell143@gmail.com>
2024-08-30 13:49:19 +0100

    refactored topics routes appearance

--- routes/topics-router.js
+++ routes/topics-router.js
@@ -3,6 +3,6 @@ const { getTopics, postTopic } = require("../controllers/topics-controller");
 const topicsRouter = require("express").Router();
 module.exports = topicsRouter;
 
-topicsRouter.get("/", getTopics);
-
-topicsRouter.post("/", postTopic);
+topicsRouter /* */
+  .get("/", getTopics)
+  .post("/", postTopic);
