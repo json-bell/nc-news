a075400030376729a242d4f1caaf0ac1db0449d5
Jason Bell <jasonbell143@gmail.com>
2024-08-30 15:32:01 +0100

    added a 'details' key to most error messages, giving some extra info on what went wrong

--- controllers/error-handling-middleware.js
+++ controllers/error-handling-middleware.js
@@ -9,20 +9,47 @@ exports.handleCustomError = (err, req, res, next) => {
 };
 
 exports.handleSqlError = (err, req, res, next) => {
-  if (
-    err.code === "22P02" || // invalid type
-    err.code === "23502" || // not null violation
-    err.code === "42703" || // column doesn't exist
-    err.code === "23505" // key already exists
-  )
-    res.status(400).send({ msg: "Bad request", code: 400 });
+  if (err.code === "22P02")
+    // invalid type
+    res.status(400).send({
+      msg: "Bad request",
+      code: 400,
+      details: "Invalid input",
+    });
+  else if (err.code === "23505")
+    // key already exists
+    res.status(400).send({
+      msg: "Bad request",
+      code: 400,
+      details: "Duplicate key",
+    });
+  else if (err.code === "23502")
+    // not null violation
+    res.status(400).send({
+      msg: "Bad request",
+      code: 400,
+      details: "Missing input",
+    });
+  else if (err.code === "42703")
+    // column doesn't exist
+    res.status(400).send({
+      msg: "Bad request",
+      code: 400,
+      details: "Invalid query",
+    });
   else if (err.code === "23503")
     // foreign key violation
-    res.status(404).send({ msg: "Resource not found", code: 404 });
+    res.status(404).send({
+      msg: "Resource not found",
+      code: 404,
+      details: "Missing dependency",
+    });
   else next(err);
 };
 
 exports.handleErrorDefault = (err, req, res, next) => {
-  console.log("Default error:", err);
+  if (process.env.NODE_ENV !== "production") {
+    console.log("Default error:", err);
+  }
   res.status(500).send({ msg: "Default error", code: 500 });
 };
